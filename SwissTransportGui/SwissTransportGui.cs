8ad5dc400c38c5dd2f44bffe6b39d4591f9362a2
silvan-bucher <silvan.bucher99@gmail.com>
2016-05-24 15:32:33 +0200

    Create SwissTransportGui

---
+++ SwissTransportGui/SwissTransportGui.cs
@@ -0,0 +1,29 @@
+﻿using SwissTransport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwissTransportGui
+{
+    public partial class SwissTransportGui : Form
+    {
+        public SwissTransportGui()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ITransport testee = new Transport();
+            //var connections = testee.GetConnections("Sursee", "Luzern");
+
+            //label1.Text = connections.ConnectionList[0].To.Station.Name;
+        }
+    }
+}
