0129879a3f8cfdae121172a1be3496055bd5bda3
silvan-bucher <silvan.bucher99@gmail.com>
2016-05-25 09:32:13 +0200

    Add basic search function to connection GUI

---
+++ SwissTransportGui/DateFormatHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissTransportGui
+{
+    class DateFormatHelper
+    {
+        public DateTime convertDuration(String duration)
+        {
+            DateTime datetime = new DateTime();
+            String days = duration.Substring(0, 2);
+            String hours = duration.Substring(3, 2);
+            String minutes = duration.Substring(6, 2);
+            String seconds = duration.Substring(9, 2);
+            //Console.WriteLine(duration);
+            //Console.WriteLine(days);
+            //Console.WriteLine(hours);
+            //Console.WriteLine(minutes);
+            //Console.WriteLine(seconds);
+            datetime = datetime.AddDays(Convert.ToDouble(days));
+            datetime = datetime.AddHours(Convert.ToDouble(hours));
+            datetime = datetime.AddMinutes(Convert.ToDouble(minutes));
+            datetime = datetime.AddSeconds(Convert.ToDouble(seconds));
+            //Console.WriteLine(datetime);
+            return datetime;
+        }
+    }
+}
--- SwissTransportGui/SwissTransportGui.cs
+++ SwissTransportGui/SwissTransportGui.cs
@@ -14,6 +14,7 @@ namespace SwissTransportGui
     public partial class SwissTransportGui : Form
     {
         ITransport transport;
+        DateFormatHelper dateFormatHelper;
         public SwissTransportGui()
         {
             InitializeComponent();
@@ -22,6 +23,7 @@ namespace SwissTransportGui
         private void Form1_Load(object sender, EventArgs e)
         {
             transport = new Transport();
+            dateFormatHelper = new DateFormatHelper();
             //ITransport testee = new Transport();
             //var connections = testee.GetConnections("Sursee", "Luzern");
 
@@ -30,14 +32,33 @@ namespace SwissTransportGui
 
         private void buttonSearchConnections_Click(object sender, EventArgs e)
         {
-            /*
             String start = textBoxFrom.Text;
             String destination = textBoxTo.Text;
 
-            Connection connectionPoint = transport.GetConnections(start, destination).ConnectionList[0];
+            //Searching connection
+            List<Connection> connections = transport.GetConnections(start, destination).ConnectionList;
+            int row = 0;
+            foreach (Connection connection in connections)
+            {
+                //Duration
+                DateTime duration = dateFormatHelper.convertDuration(connection.Duration);
+                String durationText = Convert.ToString(duration.Hour) + ":" + Convert.ToString(duration.Minute);
 
-            tableLayoutPanelConnections.Controls.Add(new Label() { Text = connectionPoint.Duration }, 0, 0);
-            */
+                //Departure
+                DateTime departure = Convert.ToDateTime(connection.From.Departure.ToString());
+                String departureText = Convert.ToString(departure.Hour + ":" + departure.Minute);
+
+                //Arrival
+                DateTime arrival = Convert.ToDateTime(connection.To.Arrival.ToString());
+                String arrivalText = Convert.ToString(arrival.Hour + ":" + arrival.Minute);
+
+                //Adding values to table
+                tableLayoutPanelConnections.Controls.Add(new Label() { Text = departureText }, 0, row);
+                tableLayoutPanelConnections.Controls.Add(new Label() { Text = arrivalText }, 1, row);
+                tableLayoutPanelConnections.Controls.Add(new Label() { Text = durationText }, 2, row);
+
+                row++;
+            }
         }
     }
 }
